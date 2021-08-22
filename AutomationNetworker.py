6970cf81f6a62bb40d0cd07b386a1c704045bf1b
Cloud User <Tiffanyyor23@Linux.qil5umf14krunfrdouw52wf0ab.bx.internal.cloudapp.net>
2021-08-22 18:04:46 +0000

    Corrected errors in code

--- AutomationNetworker.py
+++ AutomationNetworker.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask_restful import api
-from resource.recipe import RecipeListResource, RecipeResource, RecipePublishResource
+from flask_restful import Api
+from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,4 +10,4 @@ api.add_resource(RecipeResource)
 api.add_resource(RecipePublishResource)
 
 if __name__ == '__main__':
-    app.run(port 5000, debug=True)
+    app.run(port=5000, debug=True)
--- resources/recipe.py
+++ resources/recipe.py
@@ -1,5 +1,5 @@
 from flask import request
-from flask_restful import Resources
+from flask_restful import Resource
 from http import HTTPStatus
 
 from models.recipe import Recipe, recipe_list
